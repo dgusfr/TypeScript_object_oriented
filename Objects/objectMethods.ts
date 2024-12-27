60f410f84476754cff4cd562d7ebb745798c37f1
dgusfr <diegogustavo.fr@gmail.com>
2024-12-27 09:48:45 -0300

    refactor: Convert objectMethods.js to TypeScript

---
+++ Objects/objectMethods.ts
@@ -0,0 +1,5 @@
+export const getObjectKeys = (obj: object): string[] => Object.keys(obj);
+
+export const getObjectValues = (obj: object): any[] => Object.values(obj);
+
+export const getObjectEntries = (obj: object): [string, any][] => Object.entries(obj);
