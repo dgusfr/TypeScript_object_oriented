217f5086c30a4d0e0065a84b7251aabd61a985c6
dgusfr <diegogustavo.fr@gmail.com>
2024-12-27 09:47:54 -0300

    refactor: Convert objectList.js to TypeScript

---
+++ Objects/objectList.ts
@@ -0,0 +1,11 @@
+type ObjectItem = {
+  id: number;
+  name: string;
+  value: number;
+};
+
+export const objectList: ObjectItem[] = [
+  { id: 1, name: "Item 1", value: 100 },
+  { id: 2, name: "Item 2", value: 200 },
+  { id: 3, name: "Item 3", value: 300 },
+];
